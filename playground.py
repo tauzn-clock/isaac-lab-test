bfb0aa1f6516d6d7b3a65139c037be01e1b59ea4
tauzn-clock <zhongdaoxin1999@gmail.com>
2024-12-03 04:17:27 +0000

    loads usd in scene

--- playground.py
+++ playground.py
@@ -3,24 +3,30 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-"""This script demonstrates how to create a simple stage in Isaac Sim.
+"""This script demonstrates how to spawn prims into the scene.
 
 .. code-block:: bash
 
     # Usage
-    ./isaaclab.sh -p source/standalone/tutorials/00_sim/create_empty.py
+    ./isaaclab.sh -p source/standalone/tutorials/00_sim/spawn_prims.py
 
 """
 
 """Launch Isaac Sim Simulator first."""
 
+import os
+
+# Get the path of the current script
+file_path = os.path.abspath(__file__)
+directory_path = os.path.dirname(file_path)
+print(directory_path)
 
 import argparse
 
 from omni.isaac.lab.app import AppLauncher
 
 # create argparser
-parser = argparse.ArgumentParser(description="Tutorial on creating an empty stage.")
+parser = argparse.ArgumentParser(description="Tutorial on spawning prims into the scene.")
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
@@ -31,17 +37,40 @@ simulation_app = app_launcher.app
 
 """Rest everything follows."""
 
-from omni.isaac.lab.sim import SimulationCfg, SimulationContext
+import omni.isaac.core.utils.prims as prim_utils
 
+import omni.isaac.lab.sim as sim_utils
+from omni.isaac.lab.utils.assets import ISAAC_NUCLEUS_DIR
+
+
+def design_scene():
+    """Designs the scene by spawning ground plane, light, objects and meshes from usd files."""
+    # Ground-plane
+    cfg_ground = sim_utils.GroundPlaneCfg()
+    cfg_ground.func("/World/defaultGroundPlane", cfg_ground)
+
+    # spawn distant light
+    cfg_light_distant = sim_utils.DistantLightCfg(
+        intensity=3000.0,
+        color=(0.75, 0.75, 0.75),
+    )
+    cfg_light_distant.func("/World/lightDistant", cfg_light_distant, translation=(1, 0, 10))
+
+    # spawn a usd file of a table into the scene
+    cfg = sim_utils.UsdFileCfg(usd_path=f"{os.path.join(directory_path, 'grating.usdc')}")
+    cfg.func("/World/Objects/Grating", cfg, translation=(0.0, 0.0, 0))
 
 def main():
     """Main function."""
 
     # Initialize the simulation context
-    sim_cfg = SimulationCfg(dt=0.01)
-    sim = SimulationContext(sim_cfg)
+    sim_cfg = sim_utils.SimulationCfg(dt=0.01, device=args_cli.device)
+    sim = sim_utils.SimulationContext(sim_cfg)
     # Set main camera
-    sim.set_camera_view([2.5, 2.5, 2.5], [0.0, 0.0, 0.0])
+    sim.set_camera_view([2.0, 0.0, 2.5], [-0.5, 0.0, 0.5])
+
+    # Design scene by adding assets to it
+    design_scene()
 
     # Play the simulator
     sim.reset()
